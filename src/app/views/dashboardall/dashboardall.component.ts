3ede1087cb98e9c452c03a84efca7ea08bc4c61a
netvirus <yariknetvirus@gmail.com>
2021-04-20 14:27:30 +0700

    Added Dashboard All mailboxex component

---
+++ src/app/views/dashboardall/dashboardall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardall',
+  templateUrl: './dashboardall.component.html',
+  styleUrls: ['./dashboardall.component.css']
+})
+export class DashboardallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
